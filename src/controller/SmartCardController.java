781b320c473856f46ddc84f953e096953efad8ed
Sonam Rudraraju <sonam.rks@gmail.com>
2015-08-10 09:45:00 -0700

    Smart card mvc implementation for smartcard table in db

---
+++ src/controller/SmartCardController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import view.CustomerUI;
+import model.SmartCard;
+
+/**
+ *
+ * @author Sonam
+ */
+public class SmartCardController {
+    int cardNumber;
+    double balance=0;
+    
+    private SmartCard smartCard;
+    private CustomerUI customerUI;
+    
+    public SmartCardController() {
+        smartCard = new SmartCard();
+    }
+    
+    public Integer addNewCard(String name,Integer cost){
+        return smartCard.createNewCard(name,cost);
+    }
+    
+    public void removeCard(Integer num){
+        smartCard.deleteCard(num);
+    }
+    
+    public Double payUsingCard(Double price, Integer cardNumber){
+        smartCard.retrieveCardDetails(cardNumber);
+        
+        boolean balance_ok=false;
+        balance = smartCard.getBalance();
+
+            if(balance>=price){
+                balance_ok = true;
+                balance-= price;
+            }
+
+            if(balance_ok==true){
+                smartCard.setBalance(price);
+            }
+            else 
+                balance = 0.0;
+
+        return balance;
+    }
+}
+
---
+++ src/model/SmartCard.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Sonam
+ */
+public class SmartCard {
+    private Integer cardNumber;
+    private String name;
+    private Double balance;
+    
+    DatabaseConnection dbConnection = DatabaseConnection.getInstance();
+    Connection connection = dbConnection.getConnection();
+    PreparedStatement statement;
+  
+    public SmartCard() {
+    }
+    
+    public SmartCard(Integer cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public SmartCard(Integer cardNumber, String name) {
+        this.cardNumber = cardNumber;
+        this.name = name;
+    }
+
+    public Integer getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(Integer cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Double getBalance() {
+        return balance;
+    }
+    
+    public void setBalance(Double balance) {
+        try {
+            String updateBalance = "UPDATE card SET balance=? where number=?";
+            statement = connection.prepareStatement(updateBalance);
+            statement.setDouble(1,balance);
+            statement.setInt(2,cardNumber);
+            statement.executeUpdate();
+        }
+        catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        this.balance = balance;
+    }
+
+   public int getNumberOfColumns() {
+       return 2;
+   }
+   
+   public void retrieveCardDetails(Integer cardNumber) {
+       try {
+            String getCardQuery = "SELECT * FROM card where number=?";
+            statement = connection.prepareStatement(getCardQuery);
+            statement.setString(1, name);
+
+            ResultSet result = statement.executeQuery();
+
+            while(result.next()){
+                cardNumber = result.getInt("name");
+                balance = result.getDouble("price");
+            }
+        } 
+       catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+    
+    public Integer createNewCard(String name,Integer cost){
+    try {
+        String insertQuery = "INSERT INTO card VALUES(?,?,?)";
+
+        statement = connection.prepareStatement(insertQuery);
+        statement.setString(1,null);
+        statement.setString(2, name);
+        statement.setInt(3, cost);
+
+        statement.executeUpdate(); 
+
+        String getCardNumber = "SELECT number FROM card where name=?";
+        statement = connection.prepareStatement(getCardNumber);
+        statement.setString(1, name);
+
+        ResultSet result = statement.executeQuery();
+
+        while(result.next()){
+            cardNumber = result.getInt("number");
+        }
+    } 
+    catch (SQLException ex) {
+        System.out.println(ex.getMessage());
+    }
+    return cardNumber;
+}
+    
+    public void deleteCard(int num) {
+        
+    }
+}
