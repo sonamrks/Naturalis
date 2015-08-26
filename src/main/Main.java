407024337dda62da736588cd8bb088dedc6b71c7
Sonam Rudraraju <sonam.rks@gmail.com>
2015-08-26 08:45:27 -0700

    title set

--- src/main/Main.java
+++ src/main/Main.java
@@ -27,6 +27,7 @@ public class Main extends javax.swing.JFrame {
      * Creates new form Main
      */
     public Main() {
+        setTitle("Naturalis");
         itemController= new ItemController();
         cardLayout = new CardLayout();
         cards = new JPanel(cardLayout);
